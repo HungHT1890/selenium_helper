2e72b351969824d640954a2a48bbe871b083291c
blackhatmmo <hungsaki2003@hotmail.com>
2024-08-21 12:10:23 +0700

    upadte function

---
+++ selenium_helper.py
@@ -0,0 +1,97 @@
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class Navvigation:
+    def tab_new_open(self):
+        pass
+    
+    def tab_activate(self):
+        pass
+    
+    def tab_open_url(self):
+        pass
+    
+    def tab_close(self):
+        pass
+    
+    def tab_go_back(self):
+        pass
+    
+    def tab_reload(self):
+        pass
+
+
+
+class Mouse:
+    def mouse_click(self):
+        pass
+    
+    def mouse_press_hold(self):
+        pass
+    
+    def mouse_movement(self):
+        pass
+    
+    def mouse_scroll(self):
+        pass
+
+class Keyboard:
+    
+    def keyboard_type_text(self):
+        pass
+    
+    def keyboard_press_key(self):
+        pass
+
+class DataHelper:
+    def elements_finder(self):
+        pass
+    
+    def element_exists(self):
+        pass
+    
+    def element_get_url(self):
+        pass
+    
+    def element_get_text(self):
+        pass
+    
+    def element_get_value(self):
+        pass
+    
+    def element_get_attribute(self):
+        pass
+    
+    def element_sellect_dropdown(self):
+        pass
+    
+    def random_data(self):
+        pass
+
+
+
+class Untility:
+    def image_search(self):
+        pass
+    
+    def screen_short(self):
+        pass
+    
+    def switch_page(self):
+        pass
+    
+    def swith_iframe(self):
+        pass
+    
+    def swith_extension(self):
+        pass
+    
+    def execute_javascript(self):
+        pass
+    
+    def elmulate(self):
+        pass
+    
