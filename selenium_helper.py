2584d7c9c6e219b7905d0f5ca80edd469c5e3f90
blackhatmmo <hungsaki2003@hotmail.com>
2024-09-05 14:47:56 +0700

    up

--- selenium_helper.py
+++ selenium_helper.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.action_chains import ActionChins
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
